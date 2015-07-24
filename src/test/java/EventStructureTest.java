092a737b58a875dbfdddac9f35dc5fa77a82e9db
Cristian Rosa <cristian@automative.io>
2015-07-24 10:54:33 -0300

    Improve unit tests for event structure.

--- src/test/java/EventStructureTest.java
+++ src/test/java/EventStructureTest.java
@@ -3,7 +3,7 @@ import org.cifasis.mc1.EventStructure;
 import org.junit.Test;
 
 /**
- * Created by cristian on 17/07/15.
+ * Created by Cristian on 17/07/15.
  */
 public class EventStructureTest {
 
@@ -11,24 +11,25 @@ public class EventStructureTest {
     public void test1() {
         EventStructure es = new EventStructure();
         EventStructure.Event root = es.getRoot();
-
         EventStructure.Event e1 = es.newEvent("e1").dependsOn(root);
         EventStructure.Event e2 = es.newEvent("e2").dependsOn(root);
         EventStructure.Event e3 = es.newEvent("e3").dependsOn(e1).conflicts(e2);
         EventStructure.Event e4 = es.newEvent("e4").dependsOn(e3);
 
-        assert(e4.isDependent(e1));
-        assert(e4.isInConflict(e2));
-        assert(!e2.isInConflict(e1));
         System.out.println(es.toString());
-        System.out.println(e4.getCone());
-        assert(!EventStructure.isConf(Sets.newHashSet(root, e1, e2, e4)));
-        assert(es.isMaximalConf(Sets.newHashSet(root, e1, e3, e4)));
-        assert(!es.isMaximalConf(Sets.newHashSet(root, e1, e3)));
-        assert(EventStructure.isConf(Sets.newHashSet(root, e1, e2)));
-        assert(es.isMaximalConf(Sets.newHashSet(root, e1, e2)));
-        System.out.println(es.getExtensions(Sets.newHashSet(root, e1, e3)));
 
+        /* This assertions test the public API of the event structure class */
+        assert (e4.isDependent(e1));
+        assert (e4.isInConflict(e2));
+        assert (!e2.isInConflict(e1));
+        assert (e4.getCone().equals(Sets.newHashSet(root, e1, e3)));
+        assert (!EventStructure.isConf(Sets.newHashSet(root, e1, e2, e4)));
+        assert (es.isMaximalConf(Sets.newHashSet(root, e1, e3, e4)));
+        assert (!es.isMaximalConf(Sets.newHashSet(root, e1, e3)));
+        assert (EventStructure.isConf(Sets.newHashSet(root, e1, e2)));
+        assert (es.isMaximalConf(Sets.newHashSet(root, e1, e2)));
+        assert (es.getExtensions(Sets.newHashSet(root, e1, e3)).equals(Sets.newHashSet(e2, e4)));
+        assert (es.getEnabled(Sets.newHashSet(root, e1, e3)).equals(Sets.newHashSet(e4)));
+        assert (EventStructure.getMaximalEvents(Sets.newHashSet(root, e1, e2)).equals(Sets.newHashSet(e1, e2)));
     }
-
 }
