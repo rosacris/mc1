b21e3b1d93b65e7d12bd239e46edeb40c9e924a1
Cristian Rosa <cristian@automative.io>
2015-07-27 16:06:08 -0300

    Rework algorithm to cover all bounded events.

--- src/main/java/org/cifasis/mc1/Algorithm1.java
+++ src/main/java/org/cifasis/mc1/Algorithm1.java
@@ -2,8 +2,10 @@ package org.cifasis.mc1;
 
 import com.google.common.collect.Sets;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Created by Cristian on 22/07/15.
@@ -12,6 +14,7 @@ public class Algorithm1 {
 
     private final EventStructure es;                              /* the event structure to explore by the algorithm. */
     private final Set<EventStructure.Event> E;                    /* the set of events discovered by the algorithm. */
+    private final Set<EventStructure.Event> BE;                   /* the events discovered but suspended due the bound */
     private final Predicate<Set<EventStructure.Event>> contPred;  /* a predicate to decide the backtracking. */
 
     /**
@@ -25,6 +28,7 @@ public class Algorithm1 {
         this.es = es;
         this.contPred = contPred;
         this.E = Sets.newHashSet(es.getRoot());                   /* Add ⊥ to the set of discovered events */
+        this.BE = Sets.newTreeSet();
     }
 
     /**
@@ -41,7 +45,7 @@ public class Algorithm1 {
      */
     public static final Predicate<Set<EventStructure.Event>> LFS_BOUND = new Predicate<Set<EventStructure.Event>>() {
         public boolean test(Set<EventStructure.Event> conf) {
-            System.out.println("Continue?: " + conf + " , maximal: " + EventStructure.getMaximalEvents(conf));
+//            System.out.println("Continue?: " + conf + " , maximal: " + EventStructure.getMaximalEvents(conf));
             return EventStructure.getMaximalEvents(conf).size() <= 1;
         }
     };
@@ -81,27 +85,43 @@ public class Algorithm1 {
      * @param D a set of disabled events.
      * @param A a set of events to steer the exploration.
      */
-    public void explore(Set<EventStructure.Event> C, Set<EventStructure.Event> D, Set<EventStructure.Event> A) {
+    public void explore(final Set<EventStructure.Event> C, Set<EventStructure.Event> D, Set<EventStructure.Event> A) {
 
-        System.out.println(C + " | " + D + " | " + A + " ~~ " + Sets.difference(C, es.getEventSet()));
+        System.out.println(C + " | " + D + " | " + A + " ~~ " + BE);
 
-        if (es.isMaximalConf(C))
+        if (es.isMaximalConf(Sets.difference(es.getEnabled(C), D)))
             return;
 
         E.addAll(es.getExtensions(C));
 
         Set<EventStructure.Event> e;
-        if (!A.isEmpty())
-            e = Sets.newHashSet(Sets.intersection(es.getEnabled(C), A).iterator().next());
-        else
-            e = Sets.newHashSet(es.getEnabled(C).iterator().next());
+        if (!A.isEmpty()) {
+            e = Sets.newHashSet(Sets.difference(Sets.intersection(es.getEnabled(C), A), D).iterator().next());
+        } else {
+            e = Sets.newHashSet(Sets.difference(es.getEnabled(C), D).iterator().next());
+        }
 
+        Set<EventStructure.Event> boundedEvents = Sets.newTreeSet();
         if (contPred.test(Sets.union(C, e)))
             explore(Sets.union(C, e), D, Sets.difference(A, e));
+        else
+            BE.addAll(e);
 
         Set<EventStructure.Event> altConf = searchAlt(E, C, Sets.union(D, e));
-        if (!altConf.isEmpty() && contPred.test(altConf)) {
+        if (!altConf.isEmpty()) {
             explore(C, Sets.union(D, e), Sets.difference(altConf, C));
+        }
+
+        Optional<EventStructure.Event> retryConf = BE.stream().filter(new Predicate<EventStructure.Event>() {
+            public boolean test(EventStructure.Event event) {
+                Set<EventStructure.Event> maybeConf = Sets.union(C, Sets.newHashSet(event));
+                return EventStructure.isConf(maybeConf) && contPred.test(maybeConf);
+            }
+        }).findFirst();
+
+        if(retryConf.isPresent()) {
+            BE.remove(retryConf.get());
+            explore(C, Sets.union(D, e), Sets.difference(Sets.newHashSet(retryConf.get()), C));
         }
     }
 }
